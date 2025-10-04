6a040fb011d57cc8074837e7905053f49746297b
Raí Augusto <raiaugusto436@gmail.com>
2025-10-04 00:20:39 -0300

    Simple @query method

--- src/main/java/com/rai/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rai/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,14 @@ package com.rai.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.rai.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': {$regex: ?0, $options: 'i'}}")
+    List<Post> findByTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
