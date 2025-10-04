d37b2bcc37512c06fdec2693b7f5b9313536c658
Raí Augusto <raiaugusto436@gmail.com>
2025-10-03 23:49:20 -0300

    Get posts from a user and Get a post by id

---
+++ src/main/java/com/rai/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.rai.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rai.workshopmongo.domain.Post;
+import com.rai.workshopmongo.repository.PostRepository;
+import com.rai.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
